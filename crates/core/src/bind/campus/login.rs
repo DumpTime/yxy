78477ac4b0760ef9b8b9d320d957fc03ed503f08
DumpTime <85953385+DumpTime@users.noreply.github.com>
2022-11-21 12:29:28 +0800

    docs(core): fix mismatched type

--- crates/core/src/bind/campus/login.rs
+++ crates/core/src/bind/campus/login.rs
@@ -316,7 +316,7 @@ impl LoginHandler {
     /// use yxy::error::Error;
     /// # async fn run() -> Result<(), Error> {
     /// // Use the UUID of last login device
-    /// let handler = LoginHandler::build("d3ae7e7e-9c98-4498-beda-78e9e342a389")?;
+    /// let handler = LoginHandler::build("d3ae7e7e-9c98-4498-beda-78e9e342a389".to_string)?;
     /// let public_key = handler.public_key().await?;
     /// let login_info = handler.do_login_by_password("18888888888", "password", &public_key).await?;
     ///
